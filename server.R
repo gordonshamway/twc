4d9e2abf3ff5677b646b26718cb93a7ce1e75f8c
gordonshamway <stef.buchholz@googlemail.com>
2015-02-11 15:01:58 +0100

    made authentication work seamlessly

--- server.R
+++ server.R
@@ -9,6 +9,13 @@ library(wordcloud)
 shinyServer(function(input, output) {
 
     # fill the spot which we created in the ui.R
+        api_key <- "DHFmzyy7Vis9UO9cQ6tgQxVLI"
+        api_secret <- "dMbR6yGSigc8oKnnNwWNhKpYytoDNVo3aO22Jf4FCWBWZNJ96n"
+        access_token <- "68370055-zysA1i9vuvE0mP8l60miIsA8aAmhzQ2p5hW6ghkYB"
+        access_token_secret <- "Ctqn1jx2MuVBSk1XwJIkSXOATd7aNoeOaCgeZrRP0lzAq"
+        options(httr_oauth_cache=T)
+        setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+        # hier kommt immer eine Nachfrage, die muss man 
     output$plot <- renderPlot({
         
         #only when the button is pressed
